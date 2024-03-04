95b1bb3f7d4683d7dbac003fb4dadb633043d900
xinking129 <xinxin13@huawei.com>
2024-03-04 19:55:18 +0800

    更新idl脚本删除逻辑
    Signed-off-by: xinking129 <xinxin13@huawei.com>

--- scripts/idl.py
+++ scripts/idl.py
@@ -1,23 +1,7 @@
 #!/usr/bin/env python3
 # coding: utf-8
 #
-# Copyright (c) 2023 Huawei Device Co., Ltd.
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-#!/usr/bin/env python3
-# coding: utf-8
-#
-# Copyright (c) 2023 Huawei Device Co., Ltd.
+# Copyright (c) 2023-2024 Huawei Device Co., Ltd.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -40,6 +24,7 @@ def  parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--src-idl', help='idl source file')
     parser.add_argument('--dst-path', help='the converted target path')
+    parser.add_argument('--dst-file', help='the converted target file')
     parser.add_argument('--idl-tool-path', help='path of the idl conversion tool')
     parser.add_argument('--log-domainid', help='hilog domain id')
     parser.add_argument('--log-tag', help='hilog tag')
@@ -58,10 +43,16 @@ def  idl_gen_interface(input_arguments):
     (path, name) = os.path.split(input_arguments.idl_tool_path)
     is_exists = os.path.exists(input_arguments.dst_path)
     if is_exists:
-        for filename in os.listdir(input_arguments.dst_path):
-            file_path = os.path.join(input_arguments.dst_path, filename)
-            if os.path.isfile(file_path):
-                os.remove(file_path)
+        paths = input_arguments.dst_file.split(',')
+        first_path = paths[0].strip()
+        filename = os.path.basename(first_path)
+        prefix = filename.rsplit('_', maxsplit=1)[0]
+        i_prefix = 'i' + prefix
+        for file_name in os.listdir(input_arguments.dst_path):
+                if ((file_name.startswith(prefix) or file_name.startswith(i_prefix)) and
+                    (file_name.endswith('.cpp') or file_name.endswith('.h'))):
+                    file_path = os.path.join(input_arguments.dst_path, file_name)
+                    os.remove(file_path)
     is_exists = os.path.exists(input_arguments.dst_path)
 
     if not is_exists:
