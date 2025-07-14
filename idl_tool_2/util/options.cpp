9694151830854f82568fc08f69d56d981267328c
吴敏杰 <wuminjie2@h-partners.com>
2025-07-14 02:07:13 +0000

    告警修改
    Signed-off-by: 吴敏杰 <wuminjie2@h-partners.com>

--- idl_tool_2/util/options.cpp
+++ idl_tool_2/util/options.cpp
@@ -63,6 +63,10 @@ Options &Options::GetInstance()
 
 bool Options::Parse(int argc, char *argv[])
 {
+    if (argv == nullptr) {
+        Logger::E(TAG, "argc is NULL");
+        return false;
+    }
     int ret = true;
     program = argv[0];
     opterr = 0;
